ac42246fdc61c160341d94d17c9b28f45636dbda
Erica-Livia <e.ingabire1@alustudent.com>
2022-10-27 02:38:49 -0700

    Loops

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for i in range(ord('a'), ord('z')+1):
-    if {":c"}.format(i) == 'e' or {":c"}.format(i) == 'q':
+    if "{:c}".format(i) == 'e' or "{:c}".format(i) == 'q':
         continue
     else:
         print("{c}".format(i))
