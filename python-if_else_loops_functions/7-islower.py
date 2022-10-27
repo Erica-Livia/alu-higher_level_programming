015202f74a8c95db008ed606fbad0f2f89517761
Erica-Livia <e.ingabire1@alustudent.com>
2022-10-27 06:21:59 -0700

    Loops

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
-import __format__
+
 def islower(c):
-    for i in range(ord('a'), ord('z') + 1):
-        "{:c}".format(islower)
-        if "{:c}".format(islower) == True:
-            return True
-        else:
-            return False
+     if ord(c) >= 97 and ord(c) <= 122:
+         return True
+     else:
+         return False
