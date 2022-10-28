d1e07af07a77ef8618d6fa2f6adc0b7fbba9d362
Erica-Livia <e.ingabire1@alustudent.com>
2022-10-28 01:00:15 -0700

    Loops

--- python-if_else_loops_functions/100-print_tebahpla.py
+++ python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-for i in range(122, 96, -1)
-        if i % 2 == 1:
-            print("{}".format(chr(i-32)), end='')
-            continue                     
+for i in range(122, 96, -1):
+    if i % 2 == 1:
+        print("{}".format(chr(i-32)), end='')
+            continue                   
         print("{}".format(chr(i)), end='')
--- python-if_else_loops_functions/101-remove_char_at.py
+++ python-if_else_loops_functions/101-remove_char_at.py
@@ -2,4 +2,4 @@
 def remove_char_at(str, n):
         if n > len(str) - 1 or n < 0:
                     return str
-                    return str.replace(str[n], '')
+        return str.replace(str[n], '')
