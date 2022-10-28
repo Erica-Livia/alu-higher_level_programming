d8de646908e1acf7d564475d8b51712ceb8fb959
Erica-Livia <e.ingabire1@alustudent.com>
2022-10-28 01:49:14 -0700

    Modules

---
+++ python-import_modules/3-infinite_add.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+from sys import argv
+
+if __name__ == "__main__":
+    num_of_args == 0:
+    if num_of_args == 0:
+        print(0)
+    else:
+        sum = 0
+        for arg in argv[1:]
+        sum += int(arg)
+
+        print(sum)
