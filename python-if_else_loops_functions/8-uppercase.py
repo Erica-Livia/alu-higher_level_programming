e7b9c3d9e8369374d4d89e6015ba6570a9b30e69
Erica-Livia <e.ingabire1@alustudent.com>
2022-10-27 23:32:19 -0700

    Loops

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,12 +1,7 @@
 #!/usr/bin/python3
-<<<<<<< HEAD
-def uppercase(str):
-    if ord(c)
-=======
 def uppercase(a):
     for c in a:
         if 123 > ord(c) > 96:
             c = chr(ord(c) - 32)
         print("{}".format(c), end="")
     print('')
->>>>>>> e27105d6eb5f5dbf16894219bcb9156af862bf6b
