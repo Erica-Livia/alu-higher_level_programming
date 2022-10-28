a1411c1e4067244dc19ee7c7bfe49f26f4855647
Erica-Livia <e.ingabire1@alustudent.com>
2022-10-28 01:58:38 -0700

    Modules

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -2,12 +2,11 @@
 from sys import argv
 
 if __name__ == "__main__":
-    num_of_args == 0:
+    num_of_args = len(argv) - 1
     if num_of_args == 0:
         print(0)
     else:
         sum = 0
-        for arg in argv[1:]
+        for arg in argv[1:]:
             sum += int(arg)
-
         print(sum)
