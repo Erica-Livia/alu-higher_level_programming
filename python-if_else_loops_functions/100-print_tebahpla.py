aee5d543840d8528ca47b5e8f4a31748e06ab791
Erica-Livia <e.ingabire1@alustudent.com>
2022-10-28 01:02:25 -0700

    Loops

--- python-if_else_loops_functions/100-print_tebahpla.py
+++ python-if_else_loops_functions/100-print_tebahpla.py
@@ -3,4 +3,4 @@ for i in range(122, 96, -1):
     if i % 2 == 1:
         print("{}".format(chr(i-32)), end='')
             continue                   
-        print("{}".format(chr(i)), end='')
+    print("{}".format(chr(i)), end='')
