c2b9a1a1ff4d896f90ec1aa2e17e2dfaa836fe33
Erica-Livia <e.ingabire1@alustudent.com>
2022-11-04 01:12:57 -0700

    More data

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    if not my_list:
+    if my_list is not None:
         return [x if x != search else replace for x in my_list]
     return None
