a3f1a5344e90d6c695d3fea9e44d308557b6d8bd
Erica-Livia <e.ingabire1@alustudent.com>
2022-11-03 23:47:33 -0700

    Data

---
+++ python-data_structures/5-no_c.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    new_string = ""
+    for i in my_string
+        if i != 'c' and i != 'C':
+            new_string += 1
+    return my_string
---
+++ python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for i in matrix:
+        for num in i:
+            if num != i[-1]
+                print("{:d}".format(num), end=" ")
+            else:
+                print("")
