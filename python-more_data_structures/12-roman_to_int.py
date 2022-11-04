e3b73ace2b2e1e6711e24db6150bb2161f2248a0
Erica-Livia <e.ingabire1@alustudent.com>
2022-11-04 02:11:21 -0700

    More data

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -7,5 +7,5 @@ def roman_to_int(roman_string):
     roman_number = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     for r in reversed(roman_string):
         number - roman_number[r]
-        roman += number if total < number * 5 else -number
+        roman += number if roman < number * 5 else -number
     return roman
