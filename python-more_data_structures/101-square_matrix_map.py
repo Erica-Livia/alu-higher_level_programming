ca5aa5349a17ab91e49c2aa7567766d884734f52
Erica-Livia <e.ingabire1@alustudent.com>
2022-11-04 02:39:51 -0700

    More data

--- python-more_data_structures/101-square_matrix_map.py
+++ python-more_data_structures/101-square_matrix_map.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_map(matrix=[]):
-     return (list(map(lambda a: (list(map(lambda b: b**2, a))), matrix)))
+    return (list(map(lambda a: (list(map(lambda b: b**2, a))), matrix)))
