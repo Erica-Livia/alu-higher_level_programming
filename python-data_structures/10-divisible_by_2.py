de794299c18beae28189b6e8f732a89c80f4ca4d
Erica-Livia <e.ingabire1@alustudent.com>
2022-11-04 00:29:53 -0700

    Data

---
+++ python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def divisivle_by_2(my_list=[]):
+    n_list = []
+    for i in my_list:
+        if i % 2 == 0:
+            n_list.append(True)
+        else:
+            n_list.append(False)
+    return new_list
---
+++ python-data_structures/7-add_tuple.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    if len(tuple_a) < 2:
+        tuple_a = tuple_a + (0, 0)
+    if le(tuple_b) < 2:
+        tuple_b + (0, 0)
+    return (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1]
