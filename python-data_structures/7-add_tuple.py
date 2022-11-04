ce2894e2c05cbe2e98efc52cdef8de8c8f38406a
Erica-Livia <109512001+Erica-Livia@users.noreply.github.com>
2022-11-04 09:42:20 +0200

    Data

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -2,6 +2,6 @@
 def add_tuple(tuple_a=(), tuple_b=()):
     if len(tuple_a) < 2:
         tuple_a = tuple_a + (0, 0)
-    if le(tuple_b) < 2:
+    if len(tuple_b) < 2:
         tuple_b + (0, 0)
-    return (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1]
+    return (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
