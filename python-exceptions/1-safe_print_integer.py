a36cba5021292545f26bafccb0ee0e4fd4011f34
Erica-Livia <e.ingabire1@alustudent.com>
2022-11-10 01:54:17 -0800

    Exceptions

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
-    elmt = 0
-    for elmt in range(value)
-        try:
-            print("{:d}".format(), end="")
-        except IndexError:
-            elmt += 1
-    print()
-    return elmt
+    try:
+        print("{:d}".format(), end="")
+        return True
+    except:
+        return False
