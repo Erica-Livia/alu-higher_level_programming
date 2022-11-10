fa5388ef6d93dc524ea73f7b67e68e7d98b97192
Erica-Livia <e.ingabire1@alustudent.com>
2022-11-10 01:40:53 -0800

    Exceptions

---
+++ python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def safe_print_integer(value):
+    elmt = 0
+    while elmt in range(value)
+        try:
+            print("{:d}".format(), end="")
+        except IndexError:
+            elmt += 1
+    print()
+    return elmt
