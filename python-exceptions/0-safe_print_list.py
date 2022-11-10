b44205812bbd86beecbebb5c68dbf2a7aae369a3
Erica-Livia <e.ingabire1@alustudent.com>
2022-11-10 01:04:05 -0800

    Exceptions

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -3,8 +3,9 @@ def safe_print_list(my_list=[], x=0):
         elmt = 0
         for elmt in range(x):
             try:
-                print("{}".format(my_list[elmt], end=''))
+                print("{}".format(my_list[elmt]))
             except IndexError:
                 break
             elmt += 1
+            print()
         return elmt
