cc2b2aa949e03b99e9552f1d4d3fcec3ec0083f4
Erica-Livia <e.ingabire1@alustudent.com>
2022-11-10 23:16:00 -0800

    Excptions

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -1,18 +1,17 @@
 #!/usr/bin/python3
 def list_division(my_list_1, my_list_2, list_lenght):
-    elmt1 = 0
-    elmt2 =0
-    list_lenght = len(my_list_1, my_list_2)
+    div_tab = []
     for i in range(list_lenght):
             try:
                 division = my_list_1[i] / my_list_2[i]
             except:
                 division = 0
+            except TypeError:
+                print("wrong type")
+            except ZeroDivisionError:
+                print("division by 0")
+            except IndexError:
+                print("out of range")
             finally:
-                if my_list_2[i] == 0:
-                    print("division by 0")
-                elif my_list_1[i] < my_list_2[i]:
-                    print("out of range")
-                elif (my_list_1[i], my_list_2[i]) in range(list_lenght)
-                    print("wrong type")
-                return division
+                div_tab.append(division)
+    return div_tab
