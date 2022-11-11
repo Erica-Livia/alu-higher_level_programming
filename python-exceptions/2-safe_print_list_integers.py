874a326fff110dcd397b89847122f307ce56b05f
Erica-Livia <e.ingabire1@alustudent.com>
2022-11-10 22:33:01 -0800

    Exceptions

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -5,7 +5,7 @@ def safe_print_list_integers(my_list=[], x=0):
         try:
             print("{:d}".format(my_list[elmt]), end="")
             elmt += 1
-        except (IndexError, TypeError, ValueError):
+        except (TypeError, ValueError):
             continue
     print()
     return elmt
