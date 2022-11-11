3d579b3e781bb8deeac0fbb80dd130eecc1d2be7
Erica-Livia <e.ingabire1@alustudent.com>
2022-11-10 22:27:58 -0800

    Exceptions

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -3,7 +3,7 @@ def safe_print_list_integers(my_list=[], x=0):
     elmt = 0
     for elmt in range(x):
         try:
-            print("{:d}".format(my_list[i]), end="")
+            print("{:d}".format(my_list[elmt]), end="")
         except:
             continue
     print()
