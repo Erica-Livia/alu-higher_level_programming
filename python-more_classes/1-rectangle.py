025778ca39e0813b85aaece8c2ea931b694e20d5
Erica-Livia <109512001+Erica-Livia@users.noreply.github.com>
2022-11-11 21:52:52 +0200

    More classes

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -6,18 +6,17 @@ class Rectangle:
     """Rectangle"""
 
     def __init__(self, width=0, height=0):
-        """Initialization of a new rectangle"""
-        self.__width = width
-        self.__height = height
+        """Initialization of a new Rectangle"""
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
-        """Width of  the rectangle"""
+        """Width of the rectangle."""
         return self.__width
 
     @width.setter
     def width(self, value):
-        """width"""
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
@@ -26,14 +25,13 @@ class Rectangle:
 
     @property
     def height(self):
-        """height of the rectangle"""
+        """Height of the rectangle."""
         return self.__height
 
     @height.setter
     def height(self, value):
-        """Height"""
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
-        self.__height = value
+        self.__height = 
