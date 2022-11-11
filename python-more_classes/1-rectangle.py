8ca4ed8d986c3c2f84da71b238ef3ba19fed7f49
Erica-Livia <e.ingabire1@alustudent.com>
2022-11-11 10:23:17 -0800

    More Classes

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -17,6 +17,7 @@ class Rectangle:
 
     @width.setter
     def width(self, value):
+        """width"""
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
@@ -30,6 +31,7 @@ class Rectangle:
 
     @height.setter
     def height(self, value):
+        """Height"""
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
