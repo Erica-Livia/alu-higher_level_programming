4a523be86505bdc813470443500fa6c66feb9793
Erica-Livia <e.ingabire1@alustudent.com>
2022-11-11 10:07:23 -0800

    More Classes

---
+++ python-more_classes/2-rectangle.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Class Rectangle"""
+
+
+class Rectangle:
+    """Rectangle"""
+
+    def __init__(self, width=0, height=0):
+        """Initialization of a new rectangle"""
+        self.__width = width
+        self.__height = height
+
