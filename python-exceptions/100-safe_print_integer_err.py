62430f6b0c25ae2f7388be35d23a05fead2d2107
Erica-Livia <e.ingabire1@alustudent.com>
2022-11-11 00:45:58 -0800

    Exceptions

--- python-exceptions/100-safe_print_integer_err.py
+++ python-exceptions/100-safe_print_integer_err.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def safe_print_integer_err(value):
-   from sys import stderr 
-   try:
-       print("{:d}".format())
-       return True
-   except:
-       stderr.write("Exception: {}\n".format(err))
-       return False
+    from sys import stderr 
+    try:
+        print("{:d}".format())
+        return True
+    except:
+        stderr.write("Exception: {}\n".format(err))
+        return False
