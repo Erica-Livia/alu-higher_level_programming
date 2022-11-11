84350ae0b58dca22b5af0a774550d62b7a68d5d1
Erica-Livia <e.ingabire1@alustudent.com>
2022-11-11 06:18:09 -0800

    Classes

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -17,7 +17,7 @@ class Square:
     @size.setter
     def size(self, value):
         if not isinstance(value, int):
-            raise TypeError("size mut be an integer")
+            raise TypeError("size must be an integer")
         elif value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
