d4e3c7dbacab68f9e294f6d211f035dd6e40f5c6
Erica-Livia <e.ingabire1@alustudent.com>
2022-11-10 22:14:44 -0800

    Exceptions

---
+++ python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list[], x=0):
+    elmt = 0
+    for elmt in range(x):
+        try:
+            print("{}".format(my_list[i]), end="")
+        except:
+            continue
+    print()
+    return elmt
