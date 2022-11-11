96573ce5b88f6c05ea0fb430cb987fe6e3837b44
Erica-Livia <109512001+Erica-Livia@users.noreply.github.com>
2022-11-11 22:02:33 +0200

    More classes

--- python-more_classes/9-rectangle.py
+++ python-more_classes/9-rectangle.py
@@ -62,7 +62,7 @@ class Rectangle:
 
     @classmethod
     def square(cls, size=0):
-       return (cls(size, size))
+        return (cls(size, size))
 
     def __str__(self):
         if self.__width == 0 or self.__height == 0:
