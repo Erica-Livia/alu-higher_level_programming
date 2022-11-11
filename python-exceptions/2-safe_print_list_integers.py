4949a5948b6632cb5cd75a9ce357c5ed4d4e1333
Erica-Livia <e.ingabire1@alustudent.com>
2022-11-10 22:29:58 -0800

    Exceptions

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -4,7 +4,7 @@ def safe_print_list_integers(my_list=[], x=0):
     for elmt in range(x):
         try:
             print("{:d}".format(my_list[elmt]), end="")
-        except:
+        except IndexError:
             continue
     elmt += 1
     print()
