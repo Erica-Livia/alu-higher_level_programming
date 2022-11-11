dc32d0e865a634bbcf9339028ee7cdbee0f48bbd
Erica-Livia <e.ingabire1@alustudent.com>
2022-11-10 22:50:52 -0800

    Excptions

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -3,7 +3,7 @@ def safe_print_division(a, b):
     try:
         return (a / b)
     except:
-        break
+        (a / b) = None
     finally:
         print("Inside result: {}".format(a / b))
         return (a / b)
