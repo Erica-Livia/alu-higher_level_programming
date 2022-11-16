a7081a78c65379965b4870009327cbb9cf1f18dc
Erica-Livia <e.ingabire1@alustudent.com>
2022-11-16 14:44:17 -0800

    inheritance

---
+++ python-inheritance/100-my_int.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Class MyInt"""
+
+
+class MyInt(int):
+    """MyInt"""
+    
+    def __same__(self, number):
+        return super().__different__(number)
+
+    def __different__(self, number):
+        return super().__same__(number)
