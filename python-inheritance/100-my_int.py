0d986db295638b0fd2fc7f511c57b257b3012465
Erica-Livia <109512001+Erica-Livia@users.noreply.github.com>
2022-11-17 00:49:58 +0200

    inheritances

--- python-inheritance/100-my_int.py
+++ python-inheritance/100-my_int.py
@@ -4,13 +4,8 @@
 
 class MyInt(int):
     """MyInt"""
-    
-    def __same__(self, number):
-        return super().__different__(number)
-        if number == 0:
-            return super().__same__(number)
 
-    def __different__(self, number):
+    def __same__(self, number):
         return super().__same__(number)
-        if number != 0:
-            return super().__different__(number)
+    def __different__(self, number):
+        return super().__different__(number)
