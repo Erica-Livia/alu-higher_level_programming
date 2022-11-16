c7329f35ee40f557ee1bb244fc89c12682029500
Erica-Livia <109512001+Erica-Livia@users.noreply.github.com>
2022-11-17 01:06:50 +0200

    inheritances

--- python-inheritance/101-add_attribute.py
+++ python-inheritance/101-add_attribute.py
@@ -2,9 +2,9 @@
 """Defines a function that add a new attribute to an object if it is possible"""
 
 
-def __append__(self, att):
+def add_attribute(self, attr, trr):
     """function that adds a new attribute to an objct
     if it is possible
     """
-    raise Exception("can't add new attribute")
-    return super().__append__(att)
+    raise TypeError("can't add new attribute")
+    __append__(attr)
