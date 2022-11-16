7f1b731540ba1b7088f823ee5ac99b2f61167cee
Erica-Livia <e.ingabire1@alustudent.com>
2022-11-16 10:43:18 -0800

    inheritances

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -9,5 +9,5 @@ def inherits_from(obj, a_class):
     if type(obj) is not a_class and issubclass(type(obj), a_class):
         if isinstance(obj, a_class):
             return True
-        else:
-            return False
+     else:
+        return False
