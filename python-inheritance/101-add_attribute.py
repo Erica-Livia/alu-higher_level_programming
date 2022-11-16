c3bb5599c6efe8e7cb97653e86d8c1bd429c18ab
Erica-Livia <e.ingabire1@alustudent.com>
2022-11-16 15:08:55 -0800

    inheritance

--- python-inheritance/101-add_attribute.py
+++ python-inheritance/101-add_attribute.py
@@ -2,9 +2,11 @@
 """Defines a function that add a new attribute to an object if it is possible"""
 
 
-def add_attribute(self, attr, trr):
+def add_attribute(self, attr, val):
     """function that adds a new attribute to an objct
     if it is possible
     """
     raise TypeError("can't add new attribute")
     __append__(attr)
+
+    setattr(self, attr, val)
