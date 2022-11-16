2d014d190d1a612b7b0f18e34ca984e2897bfcad
Erica-Livia <e.ingabire1@alustudent.com>
2022-11-16 11:05:11 -0800

    inheritances

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""class BaseGeometry"""
+
+
+class BaseGeometry():
+    """BaseGeometry with area(self) function
+    and integer_validator(self, name, value) function
+    """
+
+    def __init__(self):
+        """initialization"""
+
+    def area(self):
+        """ raises an Exception with the message area() is not implemented"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """validates value:
+            name is always a string
+            raise a TypeError exception, with the message <name> must be an integer
+            raise a ValueError exception with the message <name> must be greater than 0
+        """
+        return (type(name) == str)
+        if type(value) is not int:
+            raise TypeError("<name> must be an integer")
+        if value is <= 0:
+            raise ValueError("<name> must be greater than 0")
