657dc5b61810a59cf8469f9ec7e76aa6ac9a5e34
Erica-Livia <e.ingabire1@alustudent.com>
2022-11-16 14:02:32 -0800

    inheritance

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -20,8 +20,7 @@ class BaseGeometry():
             raise a TypeError exception, with the message <name> must be an integer
             raise a ValueError exception with the message <name> must be greater than 0
         """
-        return (type(name) == str)
-        if type(value) is not int:
-            raise TypeError("<name> must be an integer")
+        if type(value) != int:
+            raise TypeError("{:s} must be an integer".format(name))
         if value <= 0:
-            raise ValueError("<name> must be greater than 0")
+            raise ValueError("{:s} must be greater than 0".format(name))
---
+++ python-inheritance/8-rectangle.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Rectangle Class"""
+
+
+class Rectangle():
+    """Rectangle"""
+    class BaseGeometry():
+        """BaseGeometry"""
+
+        def __init__(self, width, height):
+            """Initialization"""
+            __self.width = __import__('7-base_geometry').integer_validator
+            __self.height = __import__('7-base_geometry').integer_validator
