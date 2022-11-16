c548233d51d9e3d405ece01fe884b174d2af4ed9
Erica-Livia <e.ingabire1@alustudent.com>
2022-11-16 14:26:06 -0800

    inheritance

--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -17,4 +17,4 @@ class Rectangle(BaseGeometry):
         return self.__width * self.__height
 
     def __str__(self):
-        return ("[Rectangle]" + str(self.__width) + "/" + str(self.__height))
+        return ("[Rectangle] " + str(self.__width) + "/" + str(self.__height))
