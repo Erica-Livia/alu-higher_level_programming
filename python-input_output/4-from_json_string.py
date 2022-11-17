64c8b012ec5ac591a4a5f3ef014d8e2fb2037bcf
Erica-Livia <109512001+Erica-Livia@users.noreply.github.com>
2022-11-17 18:17:26 +0200

    input and output

--- python-input_output/4-from_json_string.py
+++ python-input_output/4-from_json_string.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """Defines a function form_json_string(my_str)"""
-import JSON
+import json
 
 
 def from_json_string(my_str):
     """This function returns an object represented by a JSON string"""
-:    return json.dumps(my_str)
+    return json.dumps(my_str)
