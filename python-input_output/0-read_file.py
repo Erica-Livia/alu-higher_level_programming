8a9ae030a899265aff9f6e49cf57d2758d74b162
Erica-Livia <e.ingabire1@alustudent.com>
2022-11-17 03:07:19 -0800

    input and output

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""defines a function read_file(filename="")"""
+
+
+def read_file(filename="UTF8"):
+    """This function reads test file
+        and prints it to stdout
+    """
+  with open('UTF8','r', encoding= "utf-8")
