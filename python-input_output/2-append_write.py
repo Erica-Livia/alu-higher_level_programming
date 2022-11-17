ca5cab05d28fc9338e89cb54e3dbca47514ecb32
Erica-Livia <e.ingabire1@alustudent.com>
2022-11-17 03:53:34 -0800

    input and output

---
+++ python-input_output/2-append_write.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""defines a function append_write(filename="", text="")"""
+
+
+def append_write(filename="", text=""):
+    """a function that appends a string
+        at the end of a the file and returns
+        the number of characters added
+    """
+    with open(filename, "a", encoding='utf-8')as f:
+        return(f.append(text))
+
