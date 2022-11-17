6e1ab8ed7059981c6e5599b1504a128c6429e9a3
Erica-Livia <109512001+Erica-Livia@users.noreply.github.com>
2022-11-17 19:37:40 +0200

    input and output

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -3,6 +3,7 @@
     and then save them to a file
 """
 import json
+from sys import argv
 save_to_json_file = __import__('5-save_to_json_file.py').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file.py').load_from_json_file
 
