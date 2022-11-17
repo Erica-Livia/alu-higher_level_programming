dc4784d4b8f013885348124d7710d33c8140ced3
Erica-Livia <e.ingabire1@alustudent.com>
2022-11-17 08:54:33 -0800

    input and output

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -2,6 +2,7 @@
 """Defines a function save_to_json_file"""
 import json
 
+
 def save_to_json_file(my_obj, filename):
     """This function writes an Object to a text file 
         using JSON representation
