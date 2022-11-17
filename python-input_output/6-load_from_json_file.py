9e330fb8ac6fbcc66f1155439aa978f90712a259
Erica-Livia <e.ingabire1@alustudent.com>
2022-11-17 09:04:39 -0800

    input and output

---
+++ python-input_output/6-load_from_json_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/pyhton3
+"""Defines a function load_from_json_file"""
+import json
+
+
+def load_from_json_file(filename):
+    """This function creates an Object from a JSON file """
+    with open(filename, encoding='utf-8') as f:
+        json.load(f)
