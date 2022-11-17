d69f8d6c3e340a9e324bc4016b5bebf893f81051
Erica-Livia <e.ingabire1@alustudent.com>
2022-11-17 03:17:51 -0800

    input and output

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -6,5 +6,6 @@ def read_file(filename="UTF8"):
     """This function reads test file
         and prints it to stdout
     """
-    with open('UTF8','r', encoding= "utf-8") as f:
-        read_data = f.read()
+    with open("UTF8") as f:
+        for line in f:
+            print(line, end="")
