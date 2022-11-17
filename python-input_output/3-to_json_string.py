1085535fc0aa1108ef6405472521eda07ada7470
Erica-Livia <e.ingabire1@alustudent.com>
2022-11-17 04:02:19 -0800

    input and output

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""defines a function to_json_string(my_obj)"""
+import json
+
+
+def to_json_string(my_obj):
+    """This function returns the JSON representation of an object(string)"""
+    return json.my_obj
