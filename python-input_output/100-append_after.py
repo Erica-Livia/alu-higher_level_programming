824c8aade360df10df276c4e3984ae009917f10c
Erica-Livia <109512001+Erica-Livia@users.noreply.github.com>
2022-11-18 02:45:29 +0200

    input and output

--- python-input_output/100-append_after.py
+++ python-input_output/100-append_after.py
@@ -1,2 +1,17 @@
 #!/usr/bin/python3
+"""defines a function append_after"""
 
+
+def append_after(filename="", search_string="", new_string=""):
+    """This function inserts a line of text to a file
+        after each line containing a specific string
+    """
+
+    with open(filename, mode="r+", encoding="utf-8") as f:
+        data = ""
+        for line in f:
+            data += line
+            if search_string in line:
+                data += new_string
+        f.seek(0)
+        f.write(data)
