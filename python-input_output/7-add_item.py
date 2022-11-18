0b4b6846faf878e106c2ddfae4acbd2a7c3e2bec
Erica-Livia <e.ingabire1@alustudent.com>
2022-11-17 16:55:16 -0800

    input and output

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -4,7 +4,6 @@
 """
 
 
-import json
 from sys import argv
 save_to_json_file = __import__('5-save_to_json_file.py').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file.py').load_from_json_file
