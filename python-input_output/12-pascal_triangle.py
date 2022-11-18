ec30acde5e24b24afad77b48976f68cb5ed0a080
Erica-Livia <109512001+Erica-Livia@users.noreply.github.com>
2022-11-18 02:43:29 +0200

    input and output

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+"""Defines a function pascal_triangle(n)"""
+
+
+def pascal_triangle(n):
+    """This function returns the list of lists of integers representing"""
+
+    if n <= 0:
+        return []
+    if n == 1:
+        return [[1]]
+
+    le = [[1]]
+    for row in range(n-1):
+        le.append([i+j for i, j in
+                   zip([0]+le[-1], le[-1] + [0])])
+    return le
