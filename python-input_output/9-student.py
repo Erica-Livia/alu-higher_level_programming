5c09d6472f116bf6b3e359459a3849305b6d9708
Erica-Livia <e.ingabire1@alustudent.com>
2022-11-17 16:53:36 -0800

    input and output

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -7,9 +7,9 @@ class Student:
 
     def __init__(self, first_name, last_name, age):
         """Initialiation"""
-        self.first_name = first_name
-        self.last_name = last_name
-        self.age = age
+        self.__first_name = first_name
+        self.__last_name = last_name
+        self.__age = age
 
     def to_json(self):
         """This function retrieves a dictionary
