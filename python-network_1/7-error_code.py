0ca21b7f383379ffaebcdafc5f58208c612e0d8d
Erica-Livia <109512001+Erica-Livia@users.noreply.github.com>
2022-11-23 10:52:09 +0200

    Network

--- python-network_1/7-error_code.py
+++ python-network_1/7-error_code.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+"""7-error_code.py"""
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    resp = requests.get(argv[1])
+    if resp.status_code >= 400:
+        print("Error code: {}".format(resp.status_code))
+    else:
+        print(resp.text)
