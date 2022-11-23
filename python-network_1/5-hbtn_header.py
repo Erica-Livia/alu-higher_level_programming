f8cf8cbbdafc1d1b482eb53e01bd75bd5bed379f
Erica-Livia <109512001+Erica-Livia@users.noreply.github.com>
2022-11-23 10:42:32 +0200

    Network

--- python-network_1/5-hbtn_header.py
+++ python-network_1/5-hbtn_header.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+"""5-hbtn_header.py"""
+from sys import argv
+import requests
+
+
+if __name__ == "__main__":
+    url = argv[1]
+    le = requests.get(url)
+    la = le.headers.get('X-Request-Id')
+    print(la)
