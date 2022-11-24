822f77d3a9e06fd33bbcc3e2edcb803f879ee74c
Erica-Livia <e.ingabire1@alustudent.com>
2022-11-24 02:17:31 -0800

    sql

--- SQL_introduction/11-best_score.sql
+++ SQL_introduction/11-best_score.sql
@@ -1,2 +1,2 @@
 -- This script lists all records with a score >= 10 in second_table
-SELECT score, name FROM second_table WHERE score >= 10;
+SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;
