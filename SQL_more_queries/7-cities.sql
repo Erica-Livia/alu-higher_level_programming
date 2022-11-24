c4d91c1cf73d86fb69422954e9867c1632e115ef
Erica-Livia <e.ingabire1@alustudent.com>
2022-11-24 08:44:47 -0800

    queries

---
+++ SQL_more_queries/7-cities.sql
@@ -0,0 +1,4 @@
+-- This script creates the database hbtn_0d_usa and the table cities
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS cities(id INT PRIMARY KEY NOT NULL AUTO_INCREMENT, state_id INT NOT NULL, name VARCHAR(256) NOT NULL, UNIQUE(id), FOREIGN KEY(state_id) REFERENCES states(id));
