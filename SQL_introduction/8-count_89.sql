905bd4c136f0ef573a3bc6c795a3a3621011e85b
Erica-Livia <e.ingabire1@alustudent.com>
2022-11-24 01:35:28 -0800

    sql

---
+++ SQL_introduction/8-count_89.sql
@@ -0,0 +1,2 @@
+-- this script displays the number of records with id = 89 in the table first_table
+SELECT COUNT(id) FROM first_table WHERE id = 89;
---
+++ SQL_introduction/9-full_creation.sql
@@ -0,0 +1,3 @@
+-- This script creates a table second_table intin thr database hbtn_0c_0
+CREATE TABLE second_table(id INT, name VARCHAR(256), score INT);
+INSERT INTO second_table (id, name, score) VALUES (1, "John", 10), (2, "Alex", 3), (3, "Bob", 14),(4, "George", 8);
