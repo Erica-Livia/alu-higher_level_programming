73a789c79eea46d171ba954f7b27d37be5b789ac
Erica-Livia <e.ingabire1@alustudent.com>
2022-11-24 15:08:04 -0800

    queries

---
+++ SQL_more_queries/16-shows_by_genre.sql
@@ -0,0 +1,5 @@
+-- This script 
+SELECT tv_shows.title, tv_genres.name FROM tv_shows
+LEFT OUTER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+LEFT OUTER JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+ORDER BY tv_shows.title, tv_genres.name;
