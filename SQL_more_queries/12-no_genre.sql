68e1db3dbc7e603c0f3d539a516b33516b123c41
Erica-Livia <e.ingabire1@alustudent.com>
2022-11-24 14:55:49 -0800

    genre

--- SQL_more_queries/12-no_genre.sql
+++ SQL_more_queries/12-no_genre.sql
@@ -1,4 +1,4 @@
 -- This script lists all shows contained in hbtn_0d_tvshows without genre
 SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows
 LEFT OUTER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
-WHERE tv_show_genres.genre_id IS NULL OREDER BY tv_shows.title, tv_show_genres.genre_id;
+WHERE tv_show_genres.genre_id IS NULL ORDER BY tv_shows.title, tv_show_genres.genre_id;
