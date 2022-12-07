6d53e058aca362ae8c528390ba0a7a700337d10a
Erica-Livia <e.ingabire1@alustudent.com>
2022-12-07 09:30:51 -0800

    tdd

---
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+""""""
+import unittest
+max_integer = __import__('6-max_integer').max_integer
+
+
+class TestMaxInteger(unittest.TestCase):
+    """Unittesrs for max_intefer([..])"""
+    def test_ordered_list(self):
+        ordered = [1, 2, 3, 4]
+        self.assertEqual(max_integer(ordered), 4)
+
+    def test_unordered_list(self):
+        unordered  [1, 2, 3, 4]
+        self.assertEqual(max_integer(unordered), 4)
+
+    def test_max_at_beginning(self):
+        max_at_beginning = [4, 3, 2, 1]
+        self.assrtEqual(max_integer(max_at_beginning), 4)
+
+    def test_empty_list(self):
+        empty = []
+        self.assertEqual(max_integer(empty), None)
+
+    def test_one_element_list(self):
+        one_element = [7]
+        self.assertEqual(max_integer(one_element), 7)
+
+    def test_floats(self):
+        floats = [1.53, 6.33, -9.123, 15.2, 6.0]
+        self.assertEqual(max_integer(floats), 15.2)
+
+    def test_ints_and_floats(self):
+        ints_and_floats = [1.53, 15.5, -9, 15, 6]
+        self.assertEqual(max_integer(ints_and_floats), 15.5)
+
+    def test_string(self):
+        string = "Brennan"
+        self.assertEqual(max_integer(string), 'r')
+
+    def test_list_of_strings(self):
+        strings = ["Brennan", "is", "my", "name"]
+        self.assertEqual(max_integer(strings), "name")
+
+    def test_empty_string(self):
+        self.assertEqual(max_integer(""), None)
+
+if __name__ == '__main__':
+    unittest.main()
