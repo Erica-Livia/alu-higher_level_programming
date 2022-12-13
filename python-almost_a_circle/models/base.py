978392a7e9fe3ea9da8054ffed3cea19487a8a6a
root <e.ingabire1@alustudent.com>
2022-12-13 00:11:06 -0800

    models

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -7,13 +7,16 @@ class Base:
     """Base
         id"""
     __nb_objects = 0
+    
     def __init__(self, id=None):
         Base.__nb_objects += 1
         self.id = id
+    
     @property
     def id(self):
         """define id"""
         return self.__id
+    
     @id.setter
     def id(self, value):
         """"id value """
@@ -21,6 +24,7 @@ class Base:
             self.__id = self.__nb_objects
         else:
             self.__id = value
+    
     @staticmethod
     def to_json_string(list_dictionaries):
         """dictionaries"""
@@ -29,6 +33,7 @@ class Base:
             return "[]"
         else:
             return json.dumps(list_dictionaries)
+    
     @classmethod
     def save_to_file(cls, list_objs):
         """ function that writes the JSON string representation of list_objs"""
@@ -40,6 +45,7 @@ class Base:
                 for obj in list_objs:
                     list_objs_dict.append(obj.to_dictionary())
                 file.write(cls.to_json_string(list_objs_dict))
+    
     @staticmethod
     def from_json_string(json_string):
         """This function returns the list of the JSON string representation"""
@@ -48,6 +54,7 @@ class Base:
             return list()
         else:
             return json.loads(json_string)
+    
     @classmethod
     def create(cls, **dictionary):
         """This function returns an instance with all attributes already set"""
@@ -57,6 +64,7 @@ class Base:
             dummy_instance = cls(4)
         dummy_instance.update(**dictionary)
         return dummy_instance
+    
     @classmethod
     def load_from_file(cls):
         """This function returns a list of instances from a file"""
