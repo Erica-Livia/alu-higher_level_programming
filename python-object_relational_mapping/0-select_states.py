b7ea4c192b9410905276b92dc938f51153c6358f
Erica-Livia <109512001+Erica-Livia@users.noreply.github.com>
2022-12-14 14:20:13 +0200

    circle

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,5 +1,18 @@
-#!/usr/bin/pyhton3
+#!/usr/bin/python3
 """0-select_states.py"""
 
 
+import MySQLdb
+from sys import argv
 
+
+if __name__ == "__main__":
+    conn = MySQLdb.connect(host="localhost", port=3306, charset="utf8",
+                           user=argv[1], passwd=argv[2], db=argv[3])
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states ORDER BY states.id ASC")
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
