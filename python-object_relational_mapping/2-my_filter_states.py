5e17d78969878287bb639b128e9a4989f892ea9e
root <e.ingabire1@alustudent.com>
2022-12-15 04:10:29 -0800

    object

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,2 +1,16 @@
 #!/usr/bin/python3
-""""""
+"""list states from databased"""
+
+
+if __name__ == "__main__":
+    import MySQLdb
+    from sys import argv
+    db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE states.name = '{}' OREDER BY states.id ASC".format(argv[4]))
+    result = cur.fetchall()
+    for i in result:
+        if i[1] == argv[4]:
+            print(i)
+    cur.close()
+    db.close()
