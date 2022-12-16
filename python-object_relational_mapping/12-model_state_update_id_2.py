005477f80e0f795d4403e64cfd05ebcc2bd3fff7
Erica-Livia <109512001+Erica-Livia@users.noreply.github.com>
2022-12-16 07:58:45 +0200

    object

--- python-object_relational_mapping/12-model_state_update_id_2.py
+++ python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,2 +1,18 @@
 #!/usr/bin/python3
 """12-model_state_update_id_2"""
+
+
+if __name__ == "__main__":
+    from sys import argv
+    from model_state import State, Base
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import sessionmaker
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        argv[1], argv[2], argv[3]), pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    Base.metadata.create_all(engine)
+    st = session.query(State).filter_by(id=2).first()
+    st.name = "New Mexico"
+    session.commit()
+    session.close()
