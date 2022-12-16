75630c568e54bd7cb1e2337be1f2263c8a21bfe8
Erica-Livia <109512001+Erica-Livia@users.noreply.github.com>
2022-12-16 07:57:59 +0200

    object

--- python-object_relational_mapping/11-model_state_insert.py
+++ python-object_relational_mapping/11-model_state_insert.py
@@ -1,2 +1,19 @@
 #!/usr/bin/python3
 """11-model_state_insert.py"""
+
+
+if __name__ == "__main__":
+    from sys import argv
+    from model_state import State, Base
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import sessionmaker
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        argv[1], argv[2], argv[3]), pool_pre_ping=True)
+    Session = sessionmaker()
+    session = Session(bind=engine)
+    Base.metadata.create_all(engine)
+    st = State(name="Louisiana")
+    session.add(st)
+    session.commit()
+    print(st.id)
+    session.close()
