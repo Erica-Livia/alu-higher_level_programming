32e16477f048c8b83578ce00914eeaa2a6f221c2
Erica-Livia <109512001+Erica-Livia@users.noreply.github.com>
2022-12-16 07:02:54 +0200

    object

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -8,8 +8,8 @@ if __name__ == "__main__":
     conn = MySQLdb.connect(host="localhost", port=3306, charset="utf8",
                            user=argv[1], passwd=argv[2], db=argv[3])
     cur = conn.cursor()
-    cur.execute("SELECT cities.id, cities.name, states.name
-                FROM cities LEFT JOIN states
+    cur.execute("SELECT cities.id, cities.name, states.name\
+                FROM cities LEFT JOIN states\
                 ON cities.state_id = states.id ORDER BY cities.id ASC")
     query_rows = cur.fetchall()
     for row in query_rows:
