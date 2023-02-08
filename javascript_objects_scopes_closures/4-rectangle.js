18996c423c6928e3204f2d0beb4bac69c162414a
ericaing <e.ingabire1@alustudent.com>
2023-02-08 02:23:50 -0800

    h

--- javascript_objects_scopes_closures/4-rectangle.js
+++ javascript_objects_scopes_closures/4-rectangle.js
@@ -1,29 +1,28 @@
 #!/usr/bin/node
 class Rectangle {
-    constructor(w,h) {
-        if (w > 0 && h > 0) {
-            this.width = w;
-            this.height = h;
-        }
-        else {
-            this.width = 0;
-            this.height = 0;
-        }    
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    } else {
+      this.width = 0;
+      this.height = 0;
     }
+  }
 
-    print() {
-        for (let i = 0; i < this.height; i++) {
-            console.log('X'.repeat(this.width));
-        }
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
     }
+  }
 
-    rotate () {
-        [this.width, this.height] = [this.height, this.width];
-    }
+  rotate () {
+    [this.width, this.height] = [this.height, this.width];
+  }
 
-    double () {
-        this.width = this.width * 2;
-        this.height = this.height * 2;
-    }
+  double () {
+    this.width = this.width * 2;
+    this.height = this.height * 2;
+  }
 }
 module.exports = Rectangle;
