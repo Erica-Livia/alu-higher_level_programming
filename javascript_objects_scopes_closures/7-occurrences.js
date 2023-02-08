1e848df34c218218390d8cc02e8feca2618ff6be
ericaing <e.ingabire1@alustudent.com>
2023-02-08 13:00:47 +0200

    occurrences

---
+++ javascript_objects_scopes_closures/7-occurrences.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+exports.nbOccurences = function (list, searchElement) {
+    return list.reduce((acc, curr) => {
+        return acc + (curr === searchElement ? 1 : 0);
+    }, 0);
+};
