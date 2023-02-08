dc4027eb71e7b5a1016139ba722b56f1fd9b7790
ericaing <e.ingabire1@alustudent.com>
2023-02-08 08:49:55 -0800

    h

--- javascript_objects_scopes_closures/6-square.js
+++ javascript_objects_scopes_closures/6-square.js
@@ -1,16 +1,16 @@
 #!/usr/bin/node
 class Square extends require('./5-square') {
-    constructor (size) {
-        super(size, size);
-    }
+  constructor (size) {
+    super(size, size);
+  }
 
-    charPrint(c) {
-       if (c == undefined) {
-        c = 'x';
-       } 
-       for (let i = 0; i < this.height; i++) {
-        console.log(c.repeat(this.width));
-       }
+  charPrint (c) {
+    if (c == undefined) {
+      c = 'x';
     }
+    for (let i = 0; i < this.height; i++) {
+      console.log(c.repeat(this.width));
+    }
+  }
 }
 module.exports = Square;
