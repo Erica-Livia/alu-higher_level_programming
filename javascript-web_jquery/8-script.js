da5eb1929f6ac7c41e44a91d5632731c0199fd07
ericaing <e.ingabire1@alustudent.com>
2023-02-10 15:56:22 +0200

    script

---
+++ javascript-web_jquery/8-script.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+$.ajax({
+    url: 'https://swapi.co/api/films/?format=json',
+    type: 'GET',
+    dataType: 'json'
+  }).done((json) => {
+    json.results.forEach((title) => {
+      $('ul#list_movies').append(`<li>${title}</li>`);
+    });
+});
---
+++ javascript-web_jquery/9-script.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+$.ajax({
+  url: 'https://fourtonfish.com/hellosalut/?lang=fr',
+  type: 'GET',
+  dataType: 'json',
+  success: (json) => {
+    $('div#hello').text(json);
+  }
+});
