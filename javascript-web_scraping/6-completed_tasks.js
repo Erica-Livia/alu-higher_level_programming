c29c0212a0a7a857778a80ee08e732625feeea8b
ericaing <e.ingabire1@alustudent.com>
2023-02-10 14:55:28 +0200

    completed tasks

---
+++ javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+const request = require('request');
+const movieId = process.argv[2];
+const url = `https://swapi.co/api/films/${movieId}`;
+request(url, function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    const characters = JSON.parse(body).characters;
+    for (const character of characters) {
+      request(character, function (error, response, body) {
+        if (error) {
+          console.log(error);
+        } else {
+          console.log(JSON.parse(body).name);
+        }
+      });
+    }
+  }
+});
