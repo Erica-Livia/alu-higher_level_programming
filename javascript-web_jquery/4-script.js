0849c7e14b32546858d6a83fab9882b70b5e07eb
ericaing <e.ingabire1@alustudent.com>
2023-02-10 15:35:43 +0200

    script

---
+++ javascript-web_jquery/4-script.js
@@ -0,0 +1,4 @@
+#!/usr/bin/node
+$('div#toggle_header').click(() => {
+    $('header').toggleClass('red green');
+});
---
+++ javascript-web_jquery/6-script.js
@@ -0,0 +1,4 @@
+#!/usr/bin/node
+$('div#update_header').click(() => {
+    $('header').text('New Header!!!');
+});
